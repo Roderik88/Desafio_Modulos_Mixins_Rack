b1a1e689f52f7f95d227801eded46acd130666da
Rodrigo <rodrigo.lindenmeyer.toledo@gmail.com>
2019-06-15 12:54:23 -0400

    second work

---
+++ metodo2.rb
@@ -0,0 +1,25 @@
+module Formula
+  def perimetro(a, b)
+    2*(a +b)
+  end
+
+  def
+class Rectangulo
+  include Formula
+  def initialize(largo, ancho)
+    @largo = largo
+    @ancho = ancho
+  end
+  def lados
+    print "#{@lado}, #{@ancho}"
+
+end
+
+class Cuadrado
+  def initialize(lado)
+    @lado = lado
+  end
+  def lados
+    print "#{@lado}"
+  end
+end
